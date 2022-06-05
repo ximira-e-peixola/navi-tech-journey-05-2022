dc54716c3c4b1cdadb6589d0d6fb70481ac87c43
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2022-06-05 19:29:02 -0300

    feat(frontend): add result card and some css ajusts

--- frontend/src/components/Sections/Map.tsx
+++ frontend/src/components/Sections/Map.tsx
@@ -1,6 +1,6 @@
 import MapboxDraw from '@mapbox/mapbox-gl-draw'
 import '@mapbox/mapbox-gl-draw/dist/mapbox-gl-draw.css'
-import { Button, Layout, Select, Row, Col } from 'antd'
+import { Button, Layout, Select, Row, Col, Card, Descriptions } from 'antd'
 import { DefaultOptionType } from 'antd/lib/select'
 import maplibregl, { IControl, Map as MapMaplibre, StyleSpecification } from 'maplibre-gl'
 import 'maplibre-gl/dist/maplibre-gl.css'
@@ -8,6 +8,7 @@ import opencage from 'opencage-api-client'
 import React, { useEffect, useRef, useState, useCallback } from 'react'
 import mapStyle from '../../assets/map/style.json'
 import MapControls from '../MapControls'
+import CardTitle from './CardTitle'
 
 const { Sider, Content } = Layout
 
@@ -22,6 +23,7 @@ export function Map () {
   const [location, setLocation] = useState<DefaultOptionType>()
   const [searchTerm, setSearchTerm] = useState('')
   const [addressOptions, setAddressOptions] = useState<DefaultOptionType[]>()
+  const [result, setResult] = useState<{ out: number, area: number, estimated_output: number, estimated_real: number, n_solar_panel: number }>()
 
   const handleDisableCalculate = useCallback(() => {
     setDisableCalculate(draw?.getAll()?.features?.length === 0)
@@ -60,7 +62,17 @@ export function Map () {
 
   const handleCalculate = useCallback(() => {
     console.log(draw.getAll())
-    fetch('/api/calculate', { headers: { 'Content-Type': 'application/json' }, method: 'POST', body: JSON.stringify({ draw: draw.getAll(), location }) }).then(resp => resp.json().then(json => console.log(json)))
+    fetch('/api/calculate',
+      {
+        headers: { 'Content-Type': 'application/json' },
+        method: 'POST',
+        body: JSON.stringify({ draw: draw.getAll(), location })
+      })
+      .then(resp => resp.json()
+        .then(json => {
+          console.log(json)
+          setResult(json)
+        }))
   }, [location])
 
   const handleDraw = useCallback(() => {
@@ -111,38 +123,75 @@ export function Map () {
 
   return (
     <div style={{ paddingTop: 80 }}>
-        <Layout>
-          <Content>
-            <div ref={mapContainer} className="map" style={{
-              width: '100%',
-              height: 'calc(100vh - 240px)'
-            }} />
-          </Content>
-          <Sider width={500} collapsedWidth={250} >
-            <Row style={{ width: '100%' }}>
-              <Col span={20}>
+      <Layout>
+        <Content>
+          <div ref={mapContainer} className="map" style={{
+            width: '100%',
+            height: 'calc(100vh - 180px)'
+          }} />
+        </Content>
+        <Sider width={500} collapsedWidth={250} style={{ backgroundColor: '#ffb80023' }}>
+          <Row style={{ width: '100%' }}>
+            <Col span={24}>
+              <Card style={{ backgroundColor: 'rgba(255, 204, 77, 0.6)', margin: 10, borderRadius: 20 }} bordered={false} title={<CardTitle />}>
+                Busque pelo seu endereço aqui:
                 <Select
-                style={{ margin: '15px 5px', width: '98%' }}
-                showSearch={true}
-                size={'large'}
-                defaultActiveFirstOption={false}
-                showArrow={false}
-                filterOption={false}
-                onSearch={handleSearch}
-                onChange={handleChange}
-                notFoundContent={null}
-                placeholder='Digite seu endereço'
-                options={addressOptions}
+                  style={{ margin: '15px 5px', width: '98%' }}
+                  showSearch={true}
+                  size={'large'}
+                  defaultActiveFirstOption={false}
+                  showArrow={false}
+                  filterOption={false}
+                  onSearch={handleSearch}
+                  onChange={handleChange}
+                  notFoundContent={null}
+                  placeholder='Digite seu endereço'
+                  options={addressOptions}
                 />
                 <Row justify='center'>
                   <Button size='large' type="primary" shape="round" onClick={handleCalculate} disabled={disableCalculate}>Calcular</Button>
-              </Row>
-              </Col>
-
-            </Row>
-
-          </Sider>
-        </Layout>
+                </Row>
+              </Card>
+              {result && (
+                <Card style={{ backgroundColor: 'rgba(255, 204, 77, 0.2)', margin: 10, borderRadius: 20 }} bordered={false} title="Seu resultado">
+                  <Row>
+                    <Col span={12}>
+                      Número de paineis solares:
+                    </Col>
+                    <Col span={12}>
+                      {result.n_solar_panel}
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={12}>
+                      Área de instalação:
+                    </Col>
+                    <Col span={12}>
+                      {result.area.toFixed(2)}m²
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={12}>
+                      Potência estimada:
+                    </Col>
+                    <Col span={12}>
+                      {result.estimated_output.toFixed(2)}kWh
+                    </Col>
+                  </Row>
+                  <Row>
+                    <Col span={12}>
+                      Produção em real:
+                    </Col>
+                    <Col span={12}>
+                      R${result.estimated_real.toFixed(2)}
+                    </Col>
+                  </Row>
+                </Card>
+              )}
+            </Col>
+          </Row>
+        </Sider>
+      </Layout>
     </div>
   )
 }
