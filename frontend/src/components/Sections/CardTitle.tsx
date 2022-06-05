faaa408b9f6c8c3b4515776820c01aa2873507ec
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2022-06-05 19:28:20 -0300

    feat(frontend): create card title

---
+++ frontend/src/components/Sections/CardTitle.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function CardTitle () {
+  return (
+        <span style={{ whiteSpace: 'normal' }}>
+            Para calcular uma estimativa de geração de energia, desenhe no mapa ao lado a área disponível para a instalação.
+        </span>
+  )
+}
