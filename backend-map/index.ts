9e15d2bc20412343e8b1e4a772de9ca8e7e44ada
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2022-06-05 16:30:13 -0300

    feat(backend-map): add return of estimated value in real

--- backend-map/index.ts
+++ backend-map/index.ts
@@ -3,6 +3,8 @@ import express from "express";
 import bodyParser from 'body-parser';
 import center from '@turf/center'
 import area from '@turf/area'
+import EstadosPorRegiao from './aneel/IBGE_EstadosPorRegiao.json'
+import TarifaMediaFornecimento from './aneel/TarifaMediaFornecimento.json'
 
 const app = express();
 app.use(bodyParser.json())
@@ -15,19 +17,23 @@ app.get('/', (req, res) => {
 This endpoint is used to calculate the estimated power output of a solar panel given the disponible area and the estimated location of the panel.
 @param {geojson} draw - The geojson of the disponible area
 
-@returns {json} - Object containing the estimated power output of the panel, the estimated GTI and the estimated area of the panel
+@returns {json} - Object containing the estimated power output of the panel, the estimated GTI, the estimated area of the panel and the estimated monthly return of the panel.
 @units {kWh/m2} - out
 @units {m2} - area
 @units {kWh} - estimated_output
+@units {R$} - estimated_real
 */
-// out: kWh/m2
-// area: m2
-// estimated_output: kWh
+
 app.post("/api/calculate", (req, res) => {
     if(!req.body.draw){
         res.status(400).send("No draw data provided");
     }
+    if(!req.body.location){
+        res.status(400).send("No location data provided");
+    }
+
     const draw = req.body.draw;
+    const location = req.body.location;
     const c = center(draw)
     const a = area(draw)
 
@@ -38,8 +44,14 @@ app.post("/api/calculate", (req, res) => {
     const coordinateTransform = new gdal.CoordinateTransformation(gdal.SpatialReference.fromEPSG(4326), dataset);
     const pt = coordinateTransform.transformPoint(pointLon, pointLat);
     const value = band.pixels.get(pt.x, pt.y)
+
+    const regiao = EstadosPorRegiao.find((estado) => estado.sigla === location.state_code || estado.nome === location.state)?.regiao?.nome
+
+    const tarifa = TarifaMediaFornecimento.find(tarifa => tarifa.nomRegiao === regiao)
+    const vlrConsumoMWh = Number(tarifa?.vlrConsumoMWh?? 0)
+    const value_real = ((value*a)/1000) * vlrConsumoMWh
     
-    res.send({ out: value, area: a, estimated_output: value * a });
+    res.send({ out: value, area: a, estimated_output: value * a, estimated_real: value_real });
 })
 
 const port = process.env.PORT || 3000;
