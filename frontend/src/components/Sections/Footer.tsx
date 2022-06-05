29047c17bf0d4f010f0eb3702c6a7bb88f2d616d
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2022-06-05 15:30:07 -0300

    feat(frontend): ajust footer

--- frontend/src/components/Sections/Footer.tsx
+++ frontend/src/components/Sections/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'react-scroll'
 // Assets
-import LogoImg from '../../assets/svg/Logo'
+import LogoImg from '../../assets/svg/LogoSolarent'
 
 const Wrapper = styled.div`
   width: 100%;
@@ -17,8 +17,11 @@ const StyleP = styled.p`
     margin: 20px 0;
   }
 `
-
-export function Footer () {
+interface FooterProps {
+  home?: boolean
+}
+export function Footer (props:FooterProps) {
+  const { home } = props
   const getCurrentYear = () => {
     return new Date().getFullYear()
   }
@@ -29,18 +32,16 @@ export function Footer () {
         <div className="container">
           <InnerWrapper className="flexSpaceCenter" style={{ padding: '30px 0' }}>
             <Link className="flexCenter animate pointer" to="home" smooth={true} offset={-80}>
-              <LogoImg />
-              <h1 className="font15 extraBold whiteColor" style={{ marginLeft: '15px' }}>
-                Fanatic
-              </h1>
+              <LogoImg invert height={40} width={170} />
             </Link>
             <StyleP className="whiteColor font13">
-              © {getCurrentYear()} - <span className="purpleColor font13">Fanatic</span> All Right Reserved
+              © {getCurrentYear()} - <span className="purpleColor font13">Solarent</span> All Right Reserved
             </StyleP>
-
-            <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
-              Back to top
-            </Link>
+            {home && (
+              <Link className="whiteColor animate pointer font13" to="home" smooth={true} offset={-80}>
+                Back to top
+              </Link>
+            )}
           </InnerWrapper>
         </div>
       </div>
