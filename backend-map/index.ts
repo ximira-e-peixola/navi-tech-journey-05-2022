46984b815021f2bedb48eb627c40abc409387373
Mateus Lehmkuhl <mateusthiesen@gmail.com>
2022-06-05 12:19:51 -0300

    feat(backend-map): change to use GTI tif and add values to return

--- backend-map/index.ts
+++ backend-map/index.ts
@@ -11,20 +11,35 @@ app.get('/', (req, res) => {
     res.send('Hello from express and typescript');
 });
 
+/*
+This endpoint is used to calculate the estimated power output of a solar panel given the disponible area and the estimated location of the panel.
+@param {geojson} draw - The geojson of the disponible area
+
+@returns {json} - Object containing the estimated power output of the panel, the estimated GTI and the estimated area of the panel
+@units {kWh/m2} - out
+@units {m2} - area
+@units {kWh} - estimated_output
+*/
+// out: kWh/m2
+// area: m2
+// estimated_output: kWh
 app.post("/api/calculate", (req, res) => {
-    const c = center(req.body.draw)
-    const a = area(req.body.draw)
-    
-    const point_lon = c.geometry.coordinates[0]
-    const point_lat = c.geometry.coordinates[1]
-    const dataset = gdal.open('./tifs/PVOUT.tif')
+    if(!req.body.draw){
+        res.status(400).send("No draw data provided");
+    }
+    const draw = req.body.draw;
+    const c = center(draw)
+    const a = area(draw)
+
+    const pointLon = c.geometry.coordinates[0]
+    const pointLat = c.geometry.coordinates[1]
+    const dataset = gdal.open('./tifs/GTI.tif')
     const band = dataset.bands.get(1)
     const coordinateTransform = new gdal.CoordinateTransformation(gdal.SpatialReference.fromEPSG(4326), dataset);
-    const pt = coordinateTransform.transformPoint(point_lon, point_lat);
+    const pt = coordinateTransform.transformPoint(pointLon, pointLat);
     const value = band.pixels.get(pt.x, pt.y)
-    console.log(pt.x, pt.y, value);
     
-    res.send({pvout: value, area: a});
+    res.send({ out: value, area: a, estimated_output: value * a });
 })
 
 const port = process.env.PORT || 3000;
